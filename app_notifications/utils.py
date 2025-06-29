56d8d85a19cefe0a57a61b3b2b16ff83723e0db7
saya <skysaika@yandex.ru>
2025-06-30 01:24:52 +0300

    Add def send_telegram_message in utils

--- app_notifications/utils.py
+++ app_notifications/utils.py
@@ -1,14 +1,22 @@
-import os
 import requests
-from dotenv import load_dotenv
+from django.conf import settings
+import logging
 
-load_dotenv()
+logger = logging.getLogger(__name__)
 
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-API_URL = f"https://api.telegram.org/bot{TOKEN}"
-
+API_URL = f"https://api.telegram.org/bot{settings.TELEGRAM_BOT_TOKEN}"
 
 def send_telegram_message(chat_id, text):
     url = f"{API_URL}/sendMessage"
-    response = requests.post(url, json={"chat_id": chat_id, "text": text})
-    return response.json()
+    payload = {
+        "chat_id": chat_id,
+        "text": text,
+    }
+    try:
+        response = requests.post(url, json=payload)
+        response.raise_for_status()
+        return response
+    except requests.RequestException as e:
+        logger.warning(f"Ошибка при отправке Telegram-сообщения в чат {chat_id}: {e}")
+        # Можно вернуть None или False, если нужно
+        return None
