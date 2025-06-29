00d23f6b7b786c51840c0a4919d2803f5d07a1af
saya <skysaika@yandex.ru>
2025-06-29 18:13:54 +0300

    Edit User model, install some packages, makemigrations and migrate

--- app_users/models.py
+++ app_users/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User(AbstractUser):
     username = None  # убираем username
     email = models.EmailField(unique=True, verbose_name='почта')
-    tg_username = models.CharField(max_length=50, unique=True, verbose_name='Имя в телеграме')
+    tg_username = models.CharField(max_length=50, unique=True, null=True, blank=True, verbose_name='Имя в телеграме')
     tg_chat_id = models.CharField(max_length=50, unique=True, null=True, blank=True, verbose_name='ID в телеграме')
 
     USERNAME_FIELD = "email"
