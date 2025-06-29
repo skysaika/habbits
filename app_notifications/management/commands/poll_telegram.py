c48e68e40f7520457971409b0d9615177687a90d
saya <skysaika@yandex.ru>
2025-06-30 01:22:07 +0300

    Add manual bot poll

--- app_notifications/management/commands/poll_telegram.py
+++ app_notifications/management/commands/poll_telegram.py
@@ -1,9 +1,9 @@
 from django.core.management.base import BaseCommand
-from app_notifications.telegram_polling import get_updates
+from app_notifications.telegram_polling import poll_telegram_updates
 
 class Command(BaseCommand):
     help = "Poll Telegram for new updates"
 
     def handle(self, *args, **kwargs):
-        get_updates()
+        poll_telegram_updates()
         self.stdout.write(self.style.SUCCESS("Polling completed."))
